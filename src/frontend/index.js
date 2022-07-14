bab07269187f4c936174b1f4f3410439c1c679ce
Kody Guffey <69737385+kozeee@users.noreply.github.com>
2022-07-14 15:34:11 -0400

    Update index.js

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,107 +1,37 @@
-"use strict";
-
+// Set up some client-side variables
 const $ = (x) => document.getElementById(x);
-
 const backendurl = "";
 
 let room;
 
-// Simple js to control when forms appear
-function gotopage(pagename) {
-  if (pagename === "getusername") {
-    $("getusername").style.display = "block";
-    $("videoroom").style.display = "none";
-    $("loading").style.display = "none";
-  } else if (pagename === "videoroom") {
-    $("getusername").style.display = "none";
-    $("videoroom").style.display = "block";
-    $("loading").style.display = "none";
-  } else {
-    $("getusername").style.display = "none";
-    $("videoroom").style.display = "none";
-    $("loading").style.display = "block";
-  }
-}
+// Create a controller that will show/hide the required forms and divs
 
-async function joinwithusername() {
-  let username = $("usernameinput").value.trim();
-  console.log("The user picked username", username);
-  gotopage("loading");
-
-  try {
-    let tokens = await axios.post(backendurl + "/get_token", {
-      user_name: username
-    });
-    let token = tokens.data.token
+// Create a function that handles our 'login' form
+  async function joinwithusername(){
+    let username = $("usernameinput").value.trim();
+    console.log(username);
     
-    try {
-      console.log("Setting up RTC session");
-      room = new SignalWire.Video.RoomSession({
-        token,
-        rootElement: document.getElementById("root"),
-        audio:true,
-        video:true
-      });
-
-      await room.join();
-      
-      room.on('member.updated',(e) => (
-        logevent(e.member.video_muted)
-        ));  
-
-    }
-     catch (error) {
-      console.error("Something went wrong", error);
-    }
-
-    gotopage("videoroom");
-  } catch (e) {
+    try{
+      let tokens = await axios.post(backendurl+"/get_token",{
+        user_name:username
+      })
+    
+    let token = tokens.data.token;
+    console.log(token)
+  }
+  catch(e){
     console.log(e);
-    alert("Error encountered. Please try again.");
-    gotopage("getusername");
   }
 }
 
-async function hangup() {
-  if (room) {
-    await room.hangup();
-    gotopage("getusername");
-  }
-}
+// Create a function to allow users to leave the call.
 
-//Start
-gotopage("getusername");
+// Create a function that allows users to share their screen
 
-let screenShareObj;
-async function share_screen() {
-    if (room === undefined) return;
-    if (screenShareObj === undefined) {
-      screenShareObj = await room.startScreenShare({
-      }
-      )
-        $("share_screen_button").innerText = "Turn off Sharing";
-    } else {
-        screenShareObj.leave();
-        screenShareObj = undefined;
-        $("share_screen_button").innerText = "Share Screen";
-    }
-}
-async function audio_mute(){
-  await room.audioMute();
-}
+// Create functions that allow users to interact with our video room
 
-async function audio_unmute(){
-  await room.audioUnmute();
-}
+  //Audio Mute and Unmute
 
-async function video_unmute(){
-  await room.videoUnmute();
-}
+  //Video Mute and Unmute
 
-async function video_mute(){
-  await room.videoMute();
-}
-
-async function join_participant(){
-}
-
+//Make sure we land on the correct page when loading
